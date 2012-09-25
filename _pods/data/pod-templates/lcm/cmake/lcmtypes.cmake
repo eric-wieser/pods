14b45736986a6be9dab496ec98801fae85e4338a
abachrac <abachrac@b50dac53-496c-468c-b28f-b3b050df618a>
2012-09-25 02:31:20 +0000

    lcmtypes_*.jar should be searched for in the opposite order since java reads the classpath left-to-right

--- _pods/data/pod-templates/lcm/cmake/lcmtypes.cmake
+++ _pods/data/pod-templates/lcm/cmake/lcmtypes.cmake
@@ -369,7 +369,7 @@ function(lcmtypes_build_java)
 
     # search for lcmtypes_*.jar files in well-known places and add them to the
     # classpath
-    foreach(pfx /usr /usr/local ${CMAKE_INSTALL_PREFIX})
+    foreach(pfx ${CMAKE_INSTALL_PREFIX} /usr /usr/local)
         file(GLOB_RECURSE jarfiles ${pfx}/share/java/lcmtypes_*.jar)
         foreach(jarfile ${jarfiles})
             set(java_classpath ${java_classpath}:${jarfile})
